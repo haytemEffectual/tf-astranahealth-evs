c322c7dfc572b6ff20bed9c12f24752eb29fa6ff
haytem <haytem.alsharif@effectual.com>
2026-01-02 22:46:05 +0000

    somechanges

---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+# here will be project specific variables
+on_premises_cidr = "192.168.0.0/16"
+transit_gateway_id = "tgw-0abcd1234efgh5678"
+vpc1_cidr = "10.1.0.0/16"
+vpc2_cidr = "10.2.0.0/16"
+domain_name = "corp.Astrana.com"
+domain_short_name = "CORP"
+ad_dns_ips = ["10.1.10.10", "10.1.10.11"]
+ad_connector_username = "svc-adconnector"   
--- variables.tf
+++ variables.tf
@@ -3,11 +3,67 @@ variable "aws_region" {
   default = "us-west-2"
 }
 
-# uncomment and set VPC_CIDR if the VPC is needed to be created via TF
+
+
+# ----------------------------------
+# Variables
+# variable "vpc1_id" {
+#   description = "VPC1 ID where AD service exists"
+#   type        = string
+# }
+# variable "vpc2_id" {
+#   description = "VPC2 ID where WorkSpaces will be deployed"
+#   type        = string
+# }
+
 variable "vpc1_cidr" {
   type = string
 }
 
 variable "vpc2_cidr" {
   type = string
+}
+
+variable "on_premises_cidr" {
+  description = "On-premises CIDR block"
+  type        = string
+  default     = "192.168.0.0/16"
+}
+variable "transit_gateway_id" {
+  description = "Transit Gateway ID"
+  type        = string
+}
+variable "domain_name" {
+  description = "Active Directory domain name"
+  type        = string
+  default     = "corp.Astrana.com"
+}
+variable "domain_short_name" {
+  description = "Active Directory short name"
+  type        = string
+  default     = "CORP"
+}
+variable "ad_dns_ips" {
+  description = "DNS IP addresses of AD servers in VPC1"
+  type        = list(string)
+  default     = ["10.1.10.10", "10.1.10.11"]
+}
+variable "ad_connector_username" {
+  description = "Service account username for AD Connector"
+  type        = string
+  default     = "svc-adconnector"
+}
+# Data sources for existing resources
+# data "aws_vpc" "vpc1" {
+#   id = var.vpc1_id
+# }
+# data "aws_vpc" "vpc2" {
+#   id = var.vpc2_id
+# }
+data "aws_availability_zones" "available" {
+  state = "available"
+  filter {
+    name   = "region-name"
+    values = ["us-west-2"]
+  }
 }
